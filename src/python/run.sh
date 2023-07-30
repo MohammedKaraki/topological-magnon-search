ad12e30e6ddf3889fc403b0eef0784efb15a020d
Mohammed Karaki <mhd.krki@gmail.com>
2023-07-30 16:17:20 -0700

    add running commands

---
+++ src/python/run.sh
@@ -0,0 +1,56 @@
+#!/bin/env bash
+
+set -x
+
+python diagnose.py 62.448 4d &&
+python diagnose.py 126.386 4d &&
+python diagnose.py 139.535 8f &&
+python diagnose.py 140.550 4a &&
+python diagnose.py 141.557 8d &&
+python diagnose.py 14.80 4a &&
+python diagnose.py 15.89 4d &&
+python diagnose.py 167.108 18e &&
+python diagnose.py 205.33 4a &&
+python diagnose.py 205.36 8b &&
+python diagnose.py 227.131 16d &&
+python diagnose.py 62.441 4b &&
+python diagnose.py 62.446 4b &&
+python diagnose.py 62.446 4c &&
+python diagnose.py 62.447 4c &&
+python diagnose.py 62.448 4b &&
+python diagnose.py 88.86 16c
+
+
+python diagnose.py 11.55 4a &&
+python diagnose.py 126.386 4d &&
+python diagnose.py 130.432 4c &&
+python diagnose.py 134.481 4d &&
+python diagnose.py 13.74 4a &&
+python diagnose.py 13.74 8f &&
+python diagnose.py 138.528 4b &&
+python diagnose.py 139.535 8f &&
+python diagnose.py 140.550 4a &&
+python diagnose.py 140.550 8g &&
+python diagnose.py 141.555 8d &&
+python diagnose.py 141.557 8d &&
+python diagnose.py 14.80 4a &&
+python diagnose.py 14.80 8e &&
+python diagnose.py 14.84 4c &&
+python diagnose.py 15.89 4c &&
+python diagnose.py 15.89 4d &&
+python diagnose.py 166.97 9e &&
+python diagnose.py 167.108 18e &&
+python diagnose.py 205.33 4a &&
+python diagnose.py 205.36 8b &&
+python diagnose.py 227.131 16d &&
+python diagnose.py 230.148 24c &&
+python diagnose.py 52.315 4b &&
+python diagnose.py 60.431 4a &&
+python diagnose.py 62.441 4b &&
+python diagnose.py 62.446 4b &&
+python diagnose.py 62.446 4c &&
+python diagnose.py 62.447 4c &&
+python diagnose.py 62.448 4a &&
+python diagnose.py 62.448 4b &&
+python diagnose.py 74.558 4b &&
+python diagnose.py 88.86 16c
