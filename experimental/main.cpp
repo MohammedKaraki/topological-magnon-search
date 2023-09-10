802b6e646226202653991daf07bcdfe002152830
Mohammed Karaki <mhd.krki@gmail.com>
2023-09-09 21:14:40 -0700

    fix compilation errors

--- experimental/main.cpp
+++ experimental/main.cpp
@@ -5,7 +5,7 @@
 #include "config/read_global_config.hpp"
 
 int main() {
-    const auto config = read_global_config();
+    const auto config = magnon::read_global_config();
     fmt::print("Cache directory: {}\n", config.cache_dir());
     boost::filesystem::path p{config.cache_dir()};
 }
