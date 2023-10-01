0e8c0522eaa9f868746c5c9d86efbc14af5635c3
Mohammed Karaki <mhd.krki@gmail.com>
2023-10-01 14:53:33 -0700

    Add python target for analyzing relevant subgroups and their tables

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 For a given material, the magnetic space group and the Wyckoff position of the magnetic atoms need to be known. Given this data, we proceed in two steps:
 1. Prefetch and preprocess data for the search by running
 ```
-bazel run //preprocess:perturb_relevant -- <MSG-NUMBER> <WP-LABEL>
+bazel run //preprocess:analyze_relevant_subgroups -- --msg_number <MSG-NUMBER> --wp <WP-LABEL>
+
 ```
 2.  Run the search algorithm with
 ```
@@ -15,7 +16,7 @@ bazel run //diagnose:diagnose -- --msg_number <MSG_NUMBER> --wp <WP> --subgroup_
 ### Example using MSG `Pa-3` and WP `4c`
 The MSG number of `Pa-3` is 205.33. For any magnetic material with this MSG & WP, we execute the following two commands:
 ```
-bazel run //preprocess:perturb_relevant -- 205.33 4c
+bazel run //preprocess:analyze_relevant_subgroups -- --msg_number 205.33 --wp 4c
 bazel run //diagnose:diagnose -- --msg_number 205.33 --wp 4c --subgroup_index 4
 ```
 
---
+++ preprocess/analyze_relevant_subgroups.py
@@ -0,0 +1,56 @@
+import sys
+import argparse
+from subprocess import check_output
+from magnon.preprocess.msg import Msg
+from magnon.preprocess.all_subgroups import gstrs_and_presc_of_subgroups
+from magnon.preprocess.msg_info_table import MSG_INFO_TABLE
+from magnon.preprocess.identify_group import identify_group
+from magnon.preprocess.perturb_wp import perturb_wp
+
+from magnon.preprocess import log
+
+
+def read_args():
+    try:
+        _, msg_number, wp = sys.argv
+    except:
+        raise ValueError("Invalid input arguments.")
+
+    return str(msg_number), str(wp)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="analyze_relevant_subgroups",
+        description="Given a magnetic space group and a Wyckoff position of the magnetic "
+        "atoms, this program filters the subgroups of the MSG for non-trivial symmetry "
+        "indicator groups. Subsequently, it fetches and analysis releveant "
+        "group representation tables for the remaining subgroups. The resulting data "
+        "can then be used by processed by the topological magnon diagnosis tool to "
+        "identify candidate materials.",
+    )
+    parser.add_argument(
+        "--msg_number",
+        required=True,
+        help="The magnetic space group number",
+    )
+    parser.add_argument(
+        "--wp",
+        required=True,
+        help="The Wyckoff position of the magnetic atoms",
+    )
+    args = parser.parse_args()
+    msg_number, wp = args.msg_number, args.wp
+
+    msg = Msg(msg_number)
+    for id, (gstrs, _) in enumerate(gstrs_and_presc_of_subgroups(msg)):
+        identified_number = identify_group(gstrs.split(";"))["group_number"]
+        si_str = MSG_INFO_TABLE[identified_number][1]
+        if si_str != "(1)":
+            print(id, identified_number, si_str, file=sys.stderr)
+            perturb_wp(msg_number, wp, str(id))
+
+
+if __name__ == "__main__":
+    logger = log.create_root_logger()
+    main()
