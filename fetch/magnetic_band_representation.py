57a74354cb00309ce626a585d2e3d19c0655ed50
Mohammed Karaki <mhd.krki@gmail.com>
2023-10-06 01:29:11 -0700

    Move atomic orbital into a separate file

--- fetch/magnetic_band_representation.py
+++ fetch/magnetic_band_representation.py
@@ -14,10 +14,12 @@ from magnon.fetch.utility.cached_requests import cached_post
 from magnon.fetch.utility.kvector import KVector
 from magnon.fetch.utility.br import LittleIrrep, Br
 from magnon.common.logger import create_logger
-from magnon.groups.magnetic_space_group_pb2 import (
+from magnon.groups.atomic_orbital_pb2 import (
     WyckoffPosition as WyckoffPositionProto,
     AtomicOrbital as AtomicOrbitalProto,
     Irrep as IrrepProto,
+)
+from magnon.groups.magnetic_space_group_pb2 import (
     AtomicBandRepresentation as AtomicBandRepresentationProto,
 )
 
