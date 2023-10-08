c889e8547b8bd91dbcddd287394f40a1ecfe5ee2
Mohammed Karaki <mhd.krki@gmail.com>
2023-10-07 23:57:55 -0700

    Add pybind bindings for reading MSGs from disk

---
+++ groups/find_subgroups_py.cpp
@@ -0,0 +1,11 @@
+#include "groups/find_subgroups.hpp"
+#include "pybind11/pybind11.h"
+#include "pybind11_protobuf/native_proto_caster.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(find_subgroups_py, m) {
+    pybind11_protobuf::ImportNativeProtoCasters();
+
+    m.def("find_subgroups", &magnon::groups::find_subgroups);
+}
---
+++ groups/read_standard_magnetic_space_groups_py.cpp
@@ -0,0 +1,12 @@
+#include "groups/read_standard_magnetic_space_groups.hpp"
+#include "pybind11/pybind11.h"
+#include "pybind11_protobuf/native_proto_caster.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(read_standard_magnetic_space_groups_py, m) {
+    pybind11_protobuf::ImportNativeProtoCasters();
+
+    m.def("read_standard_magnetic_space_groups_from_disk",
+          &magnon::groups::read_standard_magnetic_space_groups_from_disk);
+}
