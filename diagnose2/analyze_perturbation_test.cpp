0ae054e53a4a4bcc68ba0f939922d49d49a1d59f
Mohammed Karaki <mhd.krki@gmail.com>
2023-10-22 00:15:03 -0700

    Add unit tests for diagnosis

---
+++ diagnose2/analyze_perturbation_test.cpp
@@ -0,0 +1,41 @@
+#include "diagnose2/analyze_perturbation.hpp"
+
+#include <cassert>
+
+#include "common/proto_text_format.hpp"
+#include "google/protobuf/util/message_differencer.h"
+#include "gtest/gtest.h"
+
+namespace magnon::common {
+
+constexpr const char *RESULT_PATH =
+    "diagnose2/test_data/analyze_perturbation_result_205_33_4a_2_4.txtpb";
+constexpr const char *PROCESSED_TABLES_PATH =
+    "diagnose2/test_data/processed_tables_205_33_4a_2_4.txtpb";
+
+TEST(AnalyzePerturbationTest, RegressionTestCase) {
+    const auto structure = []() {
+        magnon::diagnose2::PerturbedBandStructure structure{};
+        assert(magnon::common::proto::read_from_text_file(PROCESSED_TABLES_PATH, structure));
+        return structure;
+    }();
+    const auto expected_result = []() {
+        magnon::diagnose2::SubgroupWyckoffPositionResult result{};
+        assert(magnon::common::proto::read_from_text_file(RESULT_PATH, result));
+        return result;
+    }();
+
+    const auto result = [&structure]() {
+        auto result = magnon::diagnose2::analyze_perturbation(structure);
+        result.clear_metadata();
+        return result;
+    }();
+
+    assert(result.supergroup_number() == "205.33");
+    assert(result.subgroup_number() == "2.4");
+    assert(expected_result.supergroup_number() == "205.33");
+    assert(expected_result.subgroup_number() == "2.4");
+    EXPECT_TRUE(::google::protobuf::util::MessageDifferencer::Equals(result, expected_result));
+}
+
+}  // namespace magnon::common
--- diagnose2/process_tables_test.py
+++ diagnose2/process_tables_test.py
@@ -1,6 +1,9 @@
 import unittest
 
 from magnon.diagnose2.process_tables import process_tables
+from magnon.diagnose2.perturbed_band_structure_pb2 import PerturbedBandStructure
+
+from google.protobuf import text_format
 
 """Warning: these tests are not hermetic and require internet access (when no cached data is
 available). Thus, they may break for external reasons (e.g. changes to the BCS data online).
@@ -8,10 +11,21 @@ available). Thus, they may break for external reasons (e.g. changes to the BCS d
 TODO: Refactor the fetchers to allow isolated tests.
 """
 
+_PROCESSED_TABLES_PATH = "diagnose2/test_data/processed_tables_205_33_4a_2_4.txtpb"
+
 
 class ProcessTablesTest(unittest.TestCase):
-    def test_empty(self):
-        process_tables("205.33", ["4a"], 4)
+    def test_process_table_example(self):
+        expected_structure = PerturbedBandStructure()
+        with open(_PROCESSED_TABLES_PATH, "r") as f:
+            text_format.Parse(f.read(), expected_structure)
+        assert expected_structure.supergroup.number == "205.33"
+        assert expected_structure.subgroup.number == "2.4"
+
+        structure = process_tables("205.33", ["4a"], 2)[0]
+        print(structure.supergroup)
+
+        assert structure == expected_structure
 
 
 if __name__ == "__main__":
