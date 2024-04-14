2d550d324193b0802e89540ffd92f496c61e238c
Mohammed Karaki <mhd.krki@gmail.com>
2024-04-14 10:43:46 -0700

    update make_diagram

--- WORKSPACE
+++ WORKSPACE
@@ -261,3 +261,12 @@ http_archive(
         "https://github.com/bazelbuild/buildtools/archive/refs/tags/4.2.2.tar.gz",
     ],
 )
+
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+  name = "com_google_absl",
+  urls = ["https://github.com/abseil/abseil-cpp/archive/98eb410c93ad059f9bba1bf43f5bb916fc92a5ea.zip"],
+  strip_prefix = "abseil-cpp-98eb410c93ad059f9bba1bf43f5bb916fc92a5ea",
+)
--- summary/make_diagram.cpp
+++ summary/make_diagram.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+
 #include "boost/program_options.hpp"
 #include "fmt/core.h"
 
@@ -6,31 +8,58 @@
 #include "diagnose2/spectrum_data.hpp"
 #include "summary/visualizer.hpp"
 
-namespace po = boost::program_options;
+
+struct Args {
+    Args(const int argc, const char *const argv[]);
+
+    std::string input_filename{};
+    std::string output_dir{};
+};
 
 int main(const int argc, const char *const argv[]) {
     using namespace magnon;
 
-    po::options_description desc{};
-    desc.add_options()(
-        "perturbation_data", po::value<std::string>(), "Filename of perturbed spectrum result");
-
-    po::variables_map vm{};
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    assert(vm.count("perturbation_data") == 1);
-    const auto perturbed_spectrum_filename = vm["perturbation_data"].as<std::string>();
-    const auto perturbation_data = [&]() {
-        magnon::diagnose2::PerturbedBandStructure result{};
-        magnon::common::proto::read_from_text_file(perturbed_spectrum_filename, result);
+    const Args args{argc, argv};
+    const auto perturbations = [&]() {
+        diagnose2::PerturbedBandStructures result{};
+        common::proto::read_from_text_file(args.input_filename, result);
         return result;
     }();
 
-    const diagnose2::SpectrumData spectrum_data(perturbation_data);
-    const diagnose2::Superband superband(spectrum_data.pos_neg_magnonirreps.first, spectrum_data);
-    const diagnose2::Subband subband = superband.make_subband();
+    for (const diagnose2::PerturbedBandStructure &perturbation : perturbations.structure()) {
+        const diagnose2::SpectrumData data(perturbation);
+        const diagnose2::Superband superband(data.pos_neg_magnonirreps.first, data);
+        const diagnose2::Subband subband = superband.make_subband();
 
-    Visualizer({0, 1, 2, 3, 4, 5, 6, 7}, superband, subband, spectrum_data, {}, {})
-        .dump("/home/mohammed/magnon/tmp/out.tex");
+        if (perturbation.subgroup().is_trivial_symmetry_indicator_group()) {
+            continue;
+        }
+        Visualizer({0, 1, 2, 3, 4, 5, 6, 7}, superband, subband, data, {}, {})
+            .dump("/home/mohammed/magnon/tmp/out.tex");
+    }
+}
+
+Args::Args(const int argc, const char *const argv[]) {
+    namespace po = boost::program_options;
+
+    po::options_description desc{};
+    // clang-format off
+    desc.add_options()
+        ("help", "Print help message.")
+        ("input_file", po::value(&input_filename)->required(), "Perturbations filename")
+        ("output_dir", po::value(&output_dir)->required(), "Directory for output TeX files");
+    // clang-format on
+
+    try {
+        po::variables_map vm{};
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        if (vm.count("help")) {
+            std::cout << desc;
+            std::exit(EXIT_SUCCESS);
+        }
+        po::notify(vm);
+    } catch (const po::error &e) {
+        std::cerr << "Error: " << e.what() << '\n';
+        std::exit(EXIT_SUCCESS);
+    }
 }
