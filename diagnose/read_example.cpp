4c1d181e709c684c9aa67adaa1ecda0a5c79500e
Mohammed Karaki <mhd.krki@gmail.com>
2024-05-01 01:10:24 -0700

    fix config read example

--- diagnose/read_example.cpp
+++ diagnose/read_example.cpp
@@ -8,5 +8,5 @@ int main() {
     magnon::utils::proto::read_from_text_file("config/config.cfg", config);
     // google::protobuf::TextFormat::ParseFromString(R"(cache_dir: "ASDasda")", &config);
     // std::cout << int(config.has_cache_dir()) << "\n";
-    std::cout << config.cache_dir() << "\n";
+    std::cout << config.output_base_dir() << "\n";
 }
